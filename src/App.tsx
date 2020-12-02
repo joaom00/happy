3bad1c74e46447ce28826efcf4867bafc97f1ef3
joaom <joaomljcanal@gmail.com>
2020-12-02 11:30:37 -0300

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+        </div>
+
+        <a className="enter-app" href="https://google.com">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
